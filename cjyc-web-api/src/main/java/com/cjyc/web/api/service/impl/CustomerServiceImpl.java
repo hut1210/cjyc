1060ae9066f27bbf2d483e3f59adcc050bdc1c1d
zhengjiajia <zjj5710109>
2019-09-29 16:07:40 +0800

    新增用户

--- cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/CustomerServiceImpl.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/CustomerServiceImpl.java
@@ -1,13 +1,44 @@
 package com.cjyc.web.api.service.impl;
 
+import com.cjyc.common.base.ResultEnum;
+import com.cjyc.common.base.ResultVo;
+import com.cjyc.common.model.entity.auto.Customer;
+import com.cjyc.common.model.dao.ICustomerDao;
+import com.cjyc.web.api.dto.CustomerDto;
 import com.cjyc.web.api.service.ICustomerService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
- * @auther litan
- * @description: com.cjyc.web.api.service.impl
- * @date:2019/9/28
- */
+ *  @author: zj
+ *  @Date: 2019/9/29 15:20
+ *  @Description: 移动用户
+ */ 
 @Service
+@Slf4j
+@Transactional(propagation = Propagation.REQUIRED, rollbackFor = RuntimeException.class)
 public class CustomerServiceImpl implements ICustomerService{
+
+    @Autowired
+    private ICustomerDao iCustomerDao;
+
+    @Override
+    public ResultVo saveCustomer(CustomerDto customerDto) {
+        try{
+            Customer customer = new Customer();
+            customer.setName(customerDto.getName());
+            customer.setPhone(customerDto.getPhone());
+            customer.setIdCard(customerDto.getIdCard());
+            customer.setIdCardFrontImg(customerDto.getIdCardFrontImg());
+            customer.setIdCardBackImg(customerDto.getIdCardBackImg());
+            iCustomerDao.insert(customer);
+        }catch (Exception e){
+            log.error("新增用户出现异常",e);
+            return ResultVo.response(ResultEnum.FAIL.getCode(),"新增移动用户出现异常");
+        }
+        return ResultVo.response(ResultEnum.SUCCESS.getCode(),"新增移动用户成功");
+    }
 }
