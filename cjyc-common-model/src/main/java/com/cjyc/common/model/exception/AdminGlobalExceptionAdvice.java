209eda859c0c7e18df82779bcaa2996519df1e75
zhengjiajia <zjj5710109>
2019-09-29 17:55:53 +0800

    Merge branch 'develop' of http://10.253.96.55/CJYC/cjyc into develop
    # Conflicts:
    #	cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/CustomerServiceImpl.java

--- cjyc-common-model/src/main/java/com/cjyc/common/model/exception/AdminGlobalExceptionAdvice.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/exception/AdminGlobalExceptionAdvice.java
@@ -21,7 +21,7 @@ public class AdminGlobalExceptionAdvice extends GlobalExceptionAdvice{
 
     /**
      * HttpMessageNotReadableException 需要读RequestBody时，没有传值时
-     * @param e
+     * @param e22
      * @return 400
      */
     @ExceptionHandler(value = {HttpMessageNotReadableException.class})
