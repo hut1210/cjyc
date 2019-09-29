753afb593995c74642d3a9c8300e5bc219ad9fc5
李坦 <litan@changjiulogistics.com>
2019-09-29 11:16:37 +0800

    dto vo

--- cjyc-customer-api/src/main/java/com/cjyc/customer/api/dto/res/TestRes.java
+++
@@ -1,9 +0,0 @@
-package com.cjyc.customer.api.dto.res;
-
-/**
- * @auther litan
- * @description: com.cjyc.web.api.dto.req
- * @date:2019/9/28
- */
-public class TestRes {
-}
---
+++ cjyc-salesman-api/src/main/java/com/cjyc/salesman/api/vo/TestVo.java
@@ -0,0 +1,9 @@
+package com.cjyc.salesman.api.vo;
+
+/**
+ * @auther litan
+ * @description: com.cjyc.web.api.dto.res
+ * @date:2019/9/28
+ */
+public class TestVo {
+}
