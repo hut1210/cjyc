26aa6c681f8bf2faff0d15d16d1cfc716b1cc18d
zhengjiajia <zjj5710109>
2019-09-29 18:34:51 +0800

    ....

--- cjyc-web-api/src/main/java/com/cjyc/web/api/WebApiApplication.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/WebApiApplication.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.ComponentScan;
 @MapperScan({"com.cjyc.web.api.dao","com.cjyc.common.model"})
 @ComponentScan({"com.cjyc.web.api","com.cjyc.common.model","com.cjyc.common.service"})
 public class WebApiApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(WebApiApplication.class, args);
     }
