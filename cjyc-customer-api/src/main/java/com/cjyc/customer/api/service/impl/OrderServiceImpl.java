c87991da72a45bbd9dd60db41fef4c26828a9659
李坦 <litan@changjiulogistics.com>
2019-10-08 14:38:14 +0800

    Merge branch 'develop' of http://10.253.96.55/CJYC/cjyc into develop0929
    # Conflicts:
    #	cjyc-common-service/pom.xml

--- cjyc-customer-api/src/main/java/com/cjyc/customer/api/service/impl/OrderServiceImpl.java
+++ cjyc-customer-api/src/main/java/com/cjyc/customer/api/service/impl/OrderServiceImpl.java
@@ -2,12 +2,14 @@ package com.cjyc.customer.api.service.impl;
 
 import com.cjyc.customer.api.dto.OrderDto;
 import com.cjyc.customer.api.service.IOrderService;
+import org.springframework.stereotype.Service;
 
 /**
  * @auther litan
  * @description: com.cjyc.customer.api.service.impl
  * @date:2019/10/8
  */
+@Service
 public class OrderServiceImpl implements IOrderService{
     @Override
     public boolean commitOrder(OrderDto orderDto) {
