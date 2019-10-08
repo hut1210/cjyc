64fa64513d7964af94cb8aab2254ec41ef1e6fb1
李坦 <litan@changjiulogistics.com>
2019-10-08 16:27:32 +0800

    add

--- cjyc-customer-api/src/main/java/com/cjyc/customer/api/service/impl/OrderServiceImpl.java
+++ cjyc-customer-api/src/main/java/com/cjyc/customer/api/service/impl/OrderServiceImpl.java
@@ -1,7 +1,10 @@
 package com.cjyc.customer.api.service.impl;
 
+import com.cjyc.common.model.dao.IOrderDao;
+import com.cjyc.common.model.entity.Order;
 import com.cjyc.customer.api.dto.OrderDto;
 import com.cjyc.customer.api.service.IOrderService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -11,8 +14,14 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class OrderServiceImpl implements IOrderService{
+
+    @Autowired
+    IOrderDao orderDao;
     @Override
     public boolean commitOrder(OrderDto orderDto) {
+        Order order = new Order();
+        order.setNo("555666");
+        orderDao.insert(order);
         return false;
     }
 }
