ca4d9c78a6fe0cd4447a4e1aa476c70f922c60c1
zhengjiajia <zjj5710109>
2019-10-10 11:22:49 +0800

    接收前台传过来的实体

--- cjyc-common-model/src/main/java/com/cjyc/common/model/vo/web/CustomerContractVo.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/vo/web/CustomerContractVo.java
@@ -1,73 +1,93 @@
-package com.cjyc.web.api.dto;
+package com.cjyc.common.model.vo.web;
 
 import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
+
 @Data
 public class CustomerContractVo implements Serializable {
 
     public interface SaveCustomerContractVo {
     }
+
+    public interface UpdateCustomerContractVo {
+    }
+
+    /**
+     * 合同主键id
+     */
+    private Long id;
+
     /**
      * 合同编号
      */
     @NotBlank(groups = {SaveCustomerContractVo.class},message = "合同编号不能为空")
+    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "合同编号不能为空")
     private String contractNo;
 
     /**
      * 合同性质  0：框式  1：制式
      */
     @NotNull(groups = {SaveCustomerContractVo.class},message = "合同性质不能为空")
+    @NotNull(groups = {UpdateCustomerContractVo.class},message = "合同性质不能为空")
     private Integer contactNature;
 
     /**
      * 账期 0：时付  1: 30天   2: 60天  3：90天  4：120天  5：150天
      */
     @NotNull(groups = {SaveCustomerContractVo.class},message = "账期不能为空")
+    @NotNull(groups = {UpdateCustomerContractVo.class},message = "账期不能为空")
     private Integer settlePeriod;
 
     /**
      * 合同有效期
      */
     @NotBlank(groups = {SaveCustomerContractVo.class},message = "合同有效期不能为空")
+    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "合同有效期不能为空")
     private String contractLife;
 
     /**
      * 项目名称
      */
     @NotBlank(groups = {SaveCustomerContractVo.class},message = "项目名称不能为空")
+    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "项目名称不能为空")
     private String projectName;
 
     /**
      * 项目级别  0：一级  1：二级
      */
     @NotNull(groups = {SaveCustomerContractVo.class},message = "项目级别不能为空")
+    @NotNull(groups = {UpdateCustomerContractVo.class},message = "项目级别不能为空")
     private Integer projectLevel;
 
     /**
      * 主要产品 0：新车  1：二手车 2：新车+二手车
      */
     @NotNull(groups = {SaveCustomerContractVo.class},message = "主要产品不能为空")
+    @NotNull(groups = {UpdateCustomerContractVo.class},message = "主要产品不能为空")
     private Integer majorProduct;
 
     /**
      * 项目性质 0：新开  1：存量
      */
     @NotNull(groups = {SaveCustomerContractVo.class},message = "项目性质不能为空")
+    @NotNull(groups = {UpdateCustomerContractVo.class},message = "项目性质不能为空")
     private Integer projectNature;
 
     /**
      * 项目签署日期
      */
     @NotBlank(groups = {SaveCustomerContractVo.class},message = "项目签署日期不能为空")
+    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "项目签署日期不能为空")
     private String dateOfProSign;
 
     /**
      * 一次性合同 0：否   1：是
      */
     @NotNull(groups = {SaveCustomerContractVo.class},message = "一次性合同不能为空")
+    @NotNull(groups = {UpdateCustomerContractVo.class},message = "一次性合同不能为空")
     private Integer oneOffContract;
 
     /**
@@ -94,24 +114,28 @@ public class CustomerContractVo implements Serializable {
      * 项目开发人员
      */
     @NotBlank(groups = {SaveCustomerContractVo.class},message = "项目开发人员不能为空")
+    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "项目开发人员不能为空")
     private String projectDeper;
 
     /**
      * 项目负责人
      */
     @NotBlank(groups = {SaveCustomerContractVo.class},message = "项目负责人不能为空")
+    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "项目负责人不能为空")
     private String projectLeader;
 
     /**
      * 负责人电话
      */
     @NotBlank(groups = {SaveCustomerContractVo.class},message = "负责人电话不能为空")
+    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "负责人电话不能为空")
     private String leaderPhone;
 
     /**
      * 项目状态 0：停止   1：正常
      */
     @NotNull(groups = {SaveCustomerContractVo.class},message = "项目状态不能为空")
+    @NotNull(groups = {UpdateCustomerContractVo.class},message = "项目状态不能为空")
     private Integer projectStatus;
 
     /**
--- cjyc-common-model/src/main/java/com/cjyc/common/model/vo/web/KeyCustomerVo.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/vo/web/KeyCustomerVo.java
@@ -1,4 +1,4 @@
-package com.cjyc.web.api.dto;
+package com.cjyc.common.model.vo.web;
 
 import lombok.Data;
 
@@ -12,28 +12,39 @@ public class KeyCustomerVo implements Serializable {
     public interface SaveKeyCustomerVo {
     }
 
+    public interface UpdateKeyCustomerVo {
+    }
+
+    /**
+     * 大客户主键id
+     */
+    private Long id;
     /**
      * 客户全称
      */
      @NotBlank(groups = {SaveKeyCustomerVo.class},message = "客户全称不能为空")
+     @NotBlank(groups = {UpdateKeyCustomerVo.class},message = "客户全称不能为空")
      private String name;
 
     /**
      * 客户简称
      */
     @NotBlank(groups = {SaveKeyCustomerVo.class},message = "客户简称不能为空")
+    @NotBlank(groups = {UpdateKeyCustomerVo.class},message = "客户简称不能为空")
     private String alias;
 
     /**
      * 联系人
      */
     @NotBlank(groups = {SaveKeyCustomerVo.class},message = "联系人不能为空")
+    @NotBlank(groups = {UpdateKeyCustomerVo.class},message = "联系人不能为空")
     private String contactMan;
 
     /**
      * 联系电话
      */
     @NotBlank(groups = {SaveKeyCustomerVo.class},message = "联系电话不能为空")
+    @NotBlank(groups = {UpdateKeyCustomerVo.class},message = "联系电话不能为空")
     private String phone;
 
     /**
