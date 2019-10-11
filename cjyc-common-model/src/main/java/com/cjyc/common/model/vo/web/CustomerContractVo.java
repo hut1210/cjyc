a3bd01a1a71c13cc56b081b11ff2ca009f74684d
zhengjiajia <zjj5710109>
2019-10-11 13:14:18 +0800

    调整返回实体定义

--- cjyc-common-model/src/main/java/com/cjyc/common/model/vo/web/CustomerContractVo.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/vo/web/CustomerContractVo.java
@@ -3,70 +3,42 @@ package com.cjyc.common.model.vo.web;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 public class CustomerContractVo implements Serializable {
 
-    public interface SaveCustomerContractVo {
-    }
-
-    public interface UpdateCustomerContractVo {
-    }
-
     @ApiModelProperty(value = "合同主键id")
     private Long id;
 
-    @NotBlank(groups = {SaveCustomerContractVo.class},message = "合同编号不能为空")
-    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "合同编号不能为空")
-    @ApiModelProperty(value = "合同编号",required = true)
+    @ApiModelProperty(value = "合同编号")
     private String contractNo;
 
-    @NotNull(groups = {SaveCustomerContractVo.class},message = "合同性质不能为空")
-    @NotNull(groups = {UpdateCustomerContractVo.class},message = "合同性质不能为空")
-    @ApiModelProperty(value = "合同性质 0：框式  1：制式",required = true)
+    @ApiModelProperty(value = "合同性质  0：框式  1：制式")
     private Integer contactNature;
 
-    @NotNull(groups = {SaveCustomerContractVo.class},message = "账期不能为空")
-    @NotNull(groups = {UpdateCustomerContractVo.class},message = "账期不能为空")
-    @ApiModelProperty(value = "账期 0：时付  1: 30天   2: 60天  3：90天  4：120天  5：150天",required = true)
+    @ApiModelProperty(value = "账期 0：时付  1: 30天   2: 60天  3：90天  4：120天  5：150天")
     private Integer settlePeriod;
 
-    @NotBlank(groups = {SaveCustomerContractVo.class},message = "合同有效期不能为空")
-    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "合同有效期不能为空")
-    @ApiModelProperty(value = "合同有效期",required = true)
+    @ApiModelProperty(value = "合同有效期")
     private String contractLife;
 
-    @NotBlank(groups = {SaveCustomerContractVo.class},message = "项目名称不能为空")
-    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "项目名称不能为空")
-    @ApiModelProperty(value = "项目名称",required = true)
+    @ApiModelProperty(value = "项目名称")
     private String projectName;
 
-    @NotNull(groups = {SaveCustomerContractVo.class},message = "项目级别不能为空")
-    @NotNull(groups = {UpdateCustomerContractVo.class},message = "项目级别不能为空")
-    @ApiModelProperty(value = "项目级别 0：一级  1：二级",required = true)
+    @ApiModelProperty(value = "项目级别  0：一级  1：二级")
     private Integer projectLevel;
 
-    @NotNull(groups = {SaveCustomerContractVo.class},message = "主要产品不能为空")
-    @NotNull(groups = {UpdateCustomerContractVo.class},message = "主要产品不能为空")
-    @ApiModelProperty(value = "主要产品 0：新车  1：二手车 2：新车+二手车",required = true)
+    @ApiModelProperty(value = "主要产品 0：新车  1：二手车 2：新车+二手车")
     private Integer majorProduct;
 
-    @NotNull(groups = {SaveCustomerContractVo.class},message = "项目性质不能为空")
-    @NotNull(groups = {UpdateCustomerContractVo.class},message = "项目性质不能为空")
-    @ApiModelProperty(value = "项目性质 0：新开  1：存量",required = true)
+    @ApiModelProperty(value = "项目性质 0：新开  1：存量")
     private Integer projectNature;
 
-    @NotBlank(groups = {SaveCustomerContractVo.class},message = "项目签署日期不能为空")
-    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "项目签署日期不能为空")
-    @ApiModelProperty(value = "项目签署日期",required = true)
+    @ApiModelProperty(value = "项目签署日期")
     private String dateOfProSign;
 
-    @NotNull(groups = {SaveCustomerContractVo.class},message = "一次性合同不能为空")
-    @NotNull(groups = {UpdateCustomerContractVo.class},message = "一次性合同不能为空")
-    @ApiModelProperty(value = "一次性合同 0：否   1：是",required = true)
+    @ApiModelProperty(value = "一次性合同 0：否   1：是")
     private Integer oneOffContract;
 
     @ApiModelProperty(value = "项目预计运量")
@@ -81,24 +53,16 @@ public class CustomerContractVo implements Serializable {
     @ApiModelProperty(value = "固定路线")
     private String fixedRoute;
 
-    @NotBlank(groups = {SaveCustomerContractVo.class},message = "项目开发人员不能为空")
-    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "项目开发人员不能为空")
-    @ApiModelProperty(value = "项目开发人员",required = true)
+    @ApiModelProperty(value = "项目开发人员")
     private String projectDeper;
 
-    @NotBlank(groups = {SaveCustomerContractVo.class},message = "项目负责人不能为空")
-    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "项目负责人不能为空")
-    @ApiModelProperty(value = "项目负责人",required = true)
+    @ApiModelProperty(value = "项目负责人")
     private String projectLeader;
 
-    @NotBlank(groups = {SaveCustomerContractVo.class},message = "负责人电话不能为空")
-    @NotBlank(groups = {UpdateCustomerContractVo.class},message = "负责人电话不能为空")
-    @ApiModelProperty(value = "负责人电话",required = true)
+    @ApiModelProperty(value = "负责人电话")
     private String leaderPhone;
 
-    @NotNull(groups = {SaveCustomerContractVo.class},message = "项目状态不能为空")
-    @NotNull(groups = {UpdateCustomerContractVo.class},message = "项目状态不能为空")
-    @ApiModelProperty(value = "项目状态 0：停止   1：正常",required = true)
+    @ApiModelProperty(value = "项目状态 0：停止   1：正常")
     private Integer projectStatus;
 
     @ApiModelProperty(value = "项目团队成员")
--- cjyc-common-model/src/main/java/com/cjyc/common/model/vo/web/CustomerVo.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/vo/web/CustomerVo.java
@@ -3,39 +3,26 @@ package com.cjyc.common.model.vo.web;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 
 @Data
 public class CustomerVo implements Serializable {
 
-    public interface SaveCustomerVo {
-    }
-
-    public interface UpdateCustomerVo {
-    }
-
-    @NotNull(groups = {UpdateCustomerVo.class},message = "id不能为空")
-    @ApiModelProperty(value = "主键id",required = true)
-    private Long id;
-
-    @NotBlank(groups = {SaveCustomerVo.class},message = "客户名称不能为空")
-    @NotBlank(groups = {UpdateCustomerVo.class},message = "客户名称不能为空")
-    @ApiModelProperty(value = "客户名称",required = true)
-    private String name;
-
-    @NotBlank(groups = {SaveCustomerVo.class},message = "手机号不能为空")
-    @NotBlank(groups = {UpdateCustomerVo.class},message = "手机号不能为空")
-    @ApiModelProperty(value = "手机号",required = true)
+    @ApiModelProperty(value = "账号（手机号）")
     private String phone;
+
+    @ApiModelProperty(value = "姓名")
+    private String name;
 
     @ApiModelProperty(value = "身份证号")
     private String idCard;
 
-    @ApiModelProperty(value = "身份证正面")
+    @ApiModelProperty(value = "身份证人像")
     private String idCardFrontImg;
 
-    @ApiModelProperty(value = "身份证反面")
+    @ApiModelProperty(value = "身份证反面（国徽）")
     private String idCardBackImg;
+
+    @ApiModelProperty(value = "注册时间")
+    private String registerTime;
 }
