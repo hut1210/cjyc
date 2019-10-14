4aa82b26e320ddb6c3ab3fa7c8992ad216c8e0bf
zhengjiajia <zjj5710109>
2019-10-14 11:39:08 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/entity/Incrementer.java
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/entity/OrderCar.java
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/entity/Waybill.java

--- cjyc-common-model/src/main/java/com/cjyc/common/model/dao/IUploadFileDao.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/dao/IUploadFileDao.java
@@ -3,6 +3,8 @@ package com.cjyc.common.model.dao;
 import com.cjyc.common.model.entity.UploadFile;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
+import java.util.List;
+
 /**
  * <p>
  *  Mapper 接口
@@ -12,5 +14,11 @@ import com.baomidou.mybatisplus.core.mapper.BaseMapper;
  * @since 2019-10-12
  */
 public interface IUploadFileDao extends BaseMapper<UploadFile> {
+
+    /**
+     * 获取移动端所有banner图片地址
+     * @return
+     */
+    List<String> getAllBannerPhone();
 
 }
