2ff7ab468c9df4a1e00b08f1c10d814feb194407
zhengjiajia <zjj5710109>
2019-11-11 15:52:33 +0800

    韵车2.0

--- cjyc-customer-api/src/main/java/com/cjyc/customer/api/service/impl/CustomerCouponServiceImpl.java
+++ cjyc-customer-api/src/main/java/com/cjyc/customer/api/service/impl/CustomerCouponServiceImpl.java
@@ -27,6 +27,7 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.CollectionUtils;
 
 import javax.annotation.Resource;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
@@ -45,6 +46,9 @@ public class CustomerCouponServiceImpl extends ServiceImpl<ICouponSendDao, Coupo
         Long now = LocalDateTimeUtil.getMillisByLDT(LocalDateTime.now());
         if(!CollectionUtils.isEmpty(sendVos)){
             for(CustomerCouponVo vo : sendVos){
+                vo.setFullAmount(vo.getFullAmount() == null ? BigDecimal.ZERO:vo.getFullAmount().divide(new BigDecimal(100)));
+                vo.setCutAmount(vo.getCutAmount() == null ? BigDecimal.ZERO:vo.getCutAmount().divide(new BigDecimal(100)));
+                vo.setDiscount(vo.getDiscount() == null ? BigDecimal.ZERO:vo.getDiscount());
                 if(CouponLifeTypeEnum.FOREVER.code != vo.getIsForever()){
                     if(StringUtils.isNotBlank(vo.getEndPeriodDate())){
                         Long endPeriodDate = Long.valueOf(vo.getEndPeriodDate());
