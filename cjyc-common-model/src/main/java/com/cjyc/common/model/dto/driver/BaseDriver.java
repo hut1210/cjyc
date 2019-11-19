0c4d952349bcb958e527ab5e6af93dff48da1445
zhengjiajia <zjj5710109>
2019-11-19 09:17:55 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/dao/IWaybillDao.java

--- cjyc-common-model/src/main/java/com/cjyc/common/model/dto/driver/BaseDriver.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/dto/driver/BaseDriver.java
@@ -7,7 +7,7 @@ import lombok.Data;
 import javax.validation.constraints.NotNull;
 import java.io.Serializable;
 @Data
-public class BaseDriver extends BasePageDto implements Serializable {
+public class BaseDriver extends BasePageDto {
     private static final long serialVersionUID = -640959385420785099L;
 
     @ApiModelProperty("司机id")
