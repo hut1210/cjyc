18188b9f0a6e947dffea95d9ccec13c218680286
liuxingxiang <liuxingxiang>
2019-11-26 15:13:01 +0800

    更新接口代码

--- cjyc-common-model/src/main/java/com/cjyc/common/model/vo/customer/order/OrderCenterDetailVo.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/vo/customer/order/OrderCenterDetailVo.java
@@ -26,7 +26,7 @@ public class OrderCenterDetailVo implements Serializable {
 
     @ApiModelProperty(value = "订单状态：0待提交，2待分配，5待确认，10待复确认，15待预付款，25已确认，55运输中，" +
             "88待付款，100已完成，111原返（待），112异常结束，113取消（待），114作废（待）")
-    private String state;
+    private Integer state;
 
     @ApiModelProperty(value = "下单时间")
     @JsonSerialize(using = DataLongSerizlizer.class)
