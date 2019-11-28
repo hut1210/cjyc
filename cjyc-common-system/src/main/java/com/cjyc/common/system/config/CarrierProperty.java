001179c33e0540a57248cb3e7ed23c1c28fb9faa
zhangcangman <zhangyao1988@126.com>
2019-11-28 09:49:45 +0800

    承运商新增-添加普通管理员及司机角色

--- cjyc-common-system/src/main/java/com/cjyc/common/system/config/CarrierProperty.java
+++ cjyc-common-system/src/main/java/com/cjyc/common/system/config/CarrierProperty.java
@@ -9,10 +9,19 @@ import java.util.List;
 public class CarrierProperty {
 
     public static List<Long> carriesMenuIds;
+    /**
+     * 承运商普通管理员角色资源列表
+     */
+    public static List<Long> carriesMgrMenuIds;
 
     @Value("${cjkj.carries_menu_ids}")
     public void setCarriesMenuIds(List<Long> carriesMenuIds) {
         CarrierProperty.carriesMenuIds = carriesMenuIds;
+    }
+
+    @Value("${cjkj.carries_mgr_menu_ids}")
+    public void setCarriesMgrMenuIds(List<Long> carriesMgrMenuIds) {
+        CarrierProperty.carriesMgrMenuIds = carriesMgrMenuIds;
     }
 
 }
