601caec2f0639430bcca63766517f50a4595cb6a
huteng <96531210>
2019-12-04 11:55:01 +0800

    ping++

--- cjyc-common-model/src/main/java/com/cjyc/common/model/dto/customer/pingxx/PrePayDto.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/dto/customer/pingxx/PrePayDto.java
@@ -6,12 +6,9 @@ import lombok.Data;
 @Data
 public class PrePayDto {
     @ApiModelProperty(value = "登录人ID")
-    private Long loginId;
+    private Long uid;
     @ApiModelProperty(value = "订单No")
     private String orderNo;
     @ApiModelProperty(value = "支付渠道")
     private String channel;
-
-    @ApiModelProperty(value = "IP（不用传）")
-    private String ip;
 }
