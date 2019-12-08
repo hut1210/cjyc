e23c35ed1c6761f96d63a7182885409b7fdc525f
zhengjiajia <zjj5710109>
2019-12-08 22:27:45 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/dao/IWaybillDao.java

--- cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/CustomerServiceImpl.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/CustomerServiceImpl.java
@@ -193,10 +193,10 @@ public class CustomerServiceImpl extends ServiceImpl<ICustomerDao,Customer> impl
     public ResultVo saveOrModifyKey(KeyCustomerDto dto) {
         //判断该手机号是否在库中存在
         Customer cust = customerDao.selectOne(new QueryWrapper<Customer>().lambda().eq(Customer::getContactPhone, dto.getContactPhone()));
-        if((cust != null && dto.getCustomerId() == null) || (cust != null && !cust.getId().equals(dto.getCustomerId()))){
-            return BaseResultUtil.fail("该客户已存在，请检查");
-        }
         if(dto.getCustomerId() == null){
+            if(cust != null){
+                return BaseResultUtil.fail("该客户已存在，请检查");
+            }
             //新增大客户
             Customer customer = new Customer();
             BeanUtils.copyProperties(dto,customer);
@@ -225,6 +225,9 @@ public class CustomerServiceImpl extends ServiceImpl<ICustomerDao,Customer> impl
                 customerContractService.saveBatch(list);
             }
         }else{
+            if(cust != null && !cust.getId().equals(dto.getCustomerId())){
+                return BaseResultUtil.fail("该客户已存在，请检查");
+            }
             return modifyKey(dto);
         }
         return BaseResultUtil.success();
