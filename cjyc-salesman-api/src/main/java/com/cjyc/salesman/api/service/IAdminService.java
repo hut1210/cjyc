325c1f04e7c25fd4324dfb6868a46c2be2206e6a
zhangcangman <zhangyao1988@126.com>
2019-12-09 20:32:53 +0800

    业务员查询功能

---
+++ cjyc-salesman-api/src/main/java/com/cjyc/salesman/api/service/IAdminService.java
@@ -0,0 +1,17 @@
+package com.cjyc.salesman.api.service;
+
+import com.cjyc.common.model.dto.salesman.order.SalesmanQueryDto;
+import com.cjyc.common.model.vo.PageVo;
+import com.cjyc.common.model.vo.web.admin.AdminPageVo;
+
+/**
+ * 业务员Service
+ */
+public interface IAdminService {
+    /**
+     * 分页查询
+     * @param dto
+     * @return
+     */
+    PageVo<AdminPageVo> listPage(SalesmanQueryDto dto);
+}
