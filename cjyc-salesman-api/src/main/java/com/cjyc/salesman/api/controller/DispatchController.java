2929a06c98be36ac72a5fca6a6449bd002d8a5eb
liuxingxiang <liuxingxiang>
2019-12-11 11:49:19 +0800

    更新接口代码

---
+++ cjyc-salesman-api/src/main/java/com/cjyc/salesman/api/controller/DispatchController.java
@@ -0,0 +1,17 @@
+package com.cjyc.salesman.api.controller;
+
+import io.swagger.annotations.Api;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Description 调度模块接口控制层
+ * @Author Liu Xing Xiang
+ * @Date 2019/12/11 11:25
+ **/
+@Api(tags = "调度")
+@RestController
+@RequestMapping(value = "/dispatch", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+public class DispatchController {
+}
