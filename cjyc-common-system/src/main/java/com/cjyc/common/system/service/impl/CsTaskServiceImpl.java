4a44bfa7db2081f5db9e9e9ad313a9789be3e62e
鞠培国 <jupeiguo@changjiulogistics.com>
2019-12-18 17:59:06 +0800

    调度自测

--- cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsTaskServiceImpl.java
+++ cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsTaskServiceImpl.java
@@ -290,7 +290,7 @@ public class CsTaskServiceImpl implements ICsTaskService {
         if (waybill == null) {
             return BaseResultUtil.fail("运单不存在");
         }
-        if (waybill.getState() >= WaybillStateEnum.FINISHED.code || waybill.getState() <= WaybillStateEnum.ALLOT_CONFIRM.code) {
+        if (waybill.getState() >= WaybillStateEnum.FINISHED.code || waybill.getState() < WaybillStateEnum.ALLOT_CONFIRM.code) {
             return BaseResultUtil.fail("运单已完结");
         }
 
