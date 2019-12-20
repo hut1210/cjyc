e3ec61e15653106a69bf3533cdc6602a08592c72
hut1210 <752705907@qq.com>
2019-12-20 18:09:43 +0800

    路径修改

--- cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsPingPayServiceImpl.java
+++ cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsPingPayServiceImpl.java
@@ -169,7 +169,8 @@ public class CsPingPayServiceImpl implements ICsPingPayService {
         Pingpp.apiKey = PingProperty.apiKey;
         //Pingpp.overrideApiBase("https://sapi.pingxx.com");//此接口为ping++协助测试的接口 升级完成后注释掉 20181023 add
         System.setProperty("https.protocols", "TLSv1.2");//20181023 添加 (TLSv1.2升级配置)
-        Pingpp.privateKeyPath = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX+"your_rsa_private_key_pkcs.pem").getPath();
+        //Pingpp.privateKeyPath = ResourceUtils.getFile(ResourceUtils.CLASSPATH_URL_PREFIX+"your_rsa_private_key_pkcs.pem").getPath();
+        Pingpp.privateKeyPath = CsPingPayServiceImpl.class.getClassLoader().getResource("your_rsa_private_key_pkcs.pem").getPath();
     }
 
     @Override
