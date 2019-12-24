078dc82bbd3916bccdb2596413c40c5dd53439a9
zhengjiajia <zjj5710109>
2019-12-24 16:38:33 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/dao/IStoreDao.java
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/dao/ITaskDao.java
    #	cjyc-common-model/src/main/resources/mapper/StoreMapper.xml
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/ICsStoreService.java
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsStoreServiceImpl.java

--- cjyc-common-model/src/main/java/com/cjyc/common/model/vo/ItemVo.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/vo/ItemVo.java
@@ -7,8 +7,8 @@ import java.io.Serializable;
 @Data
 public class ItemVo implements Serializable {
     private static final long serialVersionUID = -4211724368453233784L;
-    @ApiModelProperty("图片地址")
-    private String pictureUrl;
     @ApiModelProperty("H5链接地址")
+    private String pictureUrl;
+    @ApiModelProperty("地址")
     private String url;
 }
