befc8ae4f5885f56955d1dcfc517cfaeddf4f7e8
liuxingxiang <liuxingxiang>
2019-12-24 16:05:24 +0800

    更新接口代码

--- cjyc-driver-api/src/main/java/com/cjyc/driver/api/service/impl/TaskServiceImpl.java
+++ cjyc-driver-api/src/main/java/com/cjyc/driver/api/service/impl/TaskServiceImpl.java
@@ -239,9 +239,28 @@ public class TaskServiceImpl extends ServiceImpl<ITaskDao, Task> implements ITas
         if (!CollectionUtils.isEmpty(taskCarList)) {
             CarDetailVo carDetailVo = null;
             for (TaskCar taskCar : taskCarList) {
-                // 查询任务单车辆信息
-                WaybillCar waybillCar = waybillCarDao.selectById(taskCar.getWaybillCarId());
                 carDetailVo = new CarDetailVo();
+                String detailType = dto.getDetailType();
+                WaybillCar waybillCar = null;
+                // 待提车详情
+                LambdaQueryWrapper<WaybillCar> query = new QueryWrapper<WaybillCar>().lambda()
+                        .eq(WaybillCar::getId, taskCar.getWaybillCarId());
+
+                if (FieldConstant.WAIT_PICK_CAR.equals(detailType)) {
+                    // 查询任务单车辆信息
+                    query = query.eq(WaybillCar::getState,WaybillCarStateEnum.WAIT_LOAD.code)
+                            .or().eq(WaybillCar::getState,WaybillCarStateEnum.WAIT_LOAD_CONFIRM.code);
+                    waybillCar = waybillCarDao.selectOne(query);
+                }
+                // 待交车详情
+                else if(FieldConstant.WAIT_GIVE_CAR.equals(detailType)){
+                    query = query.eq(WaybillCar::getState,WaybillCarStateEnum.LOADED.code)
+                            .or().eq(WaybillCar::getState,WaybillCarStateEnum.WAIT_UNLOAD_CONFIRM.code);
+                    waybillCar = waybillCarDao.selectOne(query);
+                } else{
+                    waybillCar = waybillCarDao.selectOne(query);
+                }
+
                 BeanUtils.copyProperties(waybillCar,carDetailVo);
 
                 // 查询除了当前车辆运单的历史车辆运单图片
