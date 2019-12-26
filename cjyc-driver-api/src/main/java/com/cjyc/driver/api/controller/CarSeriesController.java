967fbf844c6d10ea48120937d0f8b6ed7994f9ca
zhengjiajia <zjj5710109>
2019-12-26 16:28:11 +0800

    Merge remote-tracking branch 'origin/develop' into develop
    # Conflicts:
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/dao/IStoreDao.java
    #	cjyc-common-model/src/main/java/com/cjyc/common/model/dao/ITaskDao.java
    #	cjyc-common-model/src/main/resources/mapper/StoreMapper.xml
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/ICsStoreService.java
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsStoreServiceImpl.java

---
+++ cjyc-driver-api/src/main/java/com/cjyc/driver/api/controller/CarSeriesController.java
@@ -0,0 +1,34 @@
+package com.cjyc.driver.api.controller;
+
+import com.cjyc.common.model.dto.KeywordDto;
+import com.cjyc.common.model.vo.ResultVo;
+import com.cjyc.common.model.vo.web.carSeries.CarSeriesTree;
+import com.cjyc.common.system.service.ICsCarSeriesService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ *  @author: zj
+ *  @Date: 2019/11/3 19:42
+ *  @Description:品牌车系
+ */
+@Api(tags = "品牌车系")
+@CrossOrigin
+@RestController
+@RequestMapping("/car")
+public class CarSeriesController {
+
+    @Resource
+    private ICsCarSeriesService csCarSeriesService;
+
+    @ApiOperation(value = "查看品牌车系")
+    @PostMapping(value = "/queryCarSeries")
+    public ResultVo<List<CarSeriesTree>> queryCarSeries(@RequestBody KeywordDto dto){
+        return csCarSeriesService.tree(true,dto.getKeyword());
+    }
+
+}
