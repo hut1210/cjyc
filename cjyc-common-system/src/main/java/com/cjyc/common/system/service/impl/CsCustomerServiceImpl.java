45d0dd3f6833de2a4c264c9cc34a8f899eae5057
zhengjiajia <zjj5710109>
2020-01-03 14:05:46 +0800

    Merge branch 'develop' of http://10.253.96.55/CJYC/cjyc into yc_auth_20200102
    # Conflicts:
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/ICsCustomerService.java
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsCustomerServiceImpl.java

--- cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsCustomerServiceImpl.java
+++ cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsCustomerServiceImpl.java
@@ -17,6 +17,7 @@ import com.cjyc.common.model.exception.ServerException;
 import com.cjyc.common.model.util.BaseResultUtil;
 import com.cjyc.common.model.util.YmlProperty;
 import com.cjyc.common.model.vo.salesman.customer.SalesCustomerVo;
+import com.cjyc.common.model.vo.salesman.mine.AppContractVo;
 import com.cjyc.common.system.feign.ISysRoleService;
 import com.cjyc.common.model.vo.ResultVo;
 import com.cjyc.common.system.feign.ISysUserService;
@@ -201,9 +202,11 @@ public class CsCustomerServiceImpl implements ICsCustomerService {
     }
 
     @Override
-    public ResultVo<List<Map<String,Object>>> findCustomerContract(AppCustomerIdDto dto) {
+    public ResultVo<AppContractVo> findCustomerContract(AppCustomerIdDto dto) {
         Long now = LocalDateTimeUtil.getMillisByLDT(LocalDateTime.now());
         List<Map<String,Object>> contractList = customerDao.getContractByCustomerId(dto.getCustomerId(),now);
-        return BaseResultUtil.success(contractList);
+        AppContractVo vo = new AppContractVo();
+        vo.setList(contractList);
+        return BaseResultUtil.success(vo);
     }
 }
