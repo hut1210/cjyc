e592cce0c31abaf8e507156d22c8b44f6d09647e
liuxingxiang <liuxingxiang>
2020-01-08 18:42:24 +0800

    更新接口代码

--- cjyc-web-api/src/main/java/com/cjyc/web/api/controller/DispatchController.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/controller/DispatchController.java
@@ -106,7 +106,7 @@ public class DispatchController {
     }
 
     @ApiOperation(value = "根据订单车辆ID查询可调度起始地和目的地")
-    @PostMapping(value = "/car/from/to/get")
+    //@PostMapping(value = "/car/from/to/get")
     public ResultVo validateCanDispatch(@RequestBody ComputeCarEndpointDto reqDto) {
         return csOrderService.validateCanDispatch(reqDto);
     }
