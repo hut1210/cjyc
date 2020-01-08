a858767906f29b62777c63ab831f8e4a46ece70a
liuxingxiang <liuxingxiang>
2020-01-08 13:58:38 +0800

    更新接口代码

--- cjyc-salesman-api/src/main/java/com/cjyc/salesman/api/service/impl/TaskServiceImpl.java
+++ cjyc-salesman-api/src/main/java/com/cjyc/salesman/api/service/impl/TaskServiceImpl.java
@@ -130,11 +130,9 @@ public class TaskServiceImpl implements ITaskService {
                     String logoImg = carSeriesDao.getLogoImgByBraMod(carDetailVo.getBrand(),carDetailVo.getModel());
                     carDetailVo.setLogoPhotoImg(LogoImgProperty.logoImg+logoImg);
 
-                    // 查询订单信息
+                    // 查询支付类型
                     Order order = orderDao.selectById(orderCar.getOrderId());
                     carDetailVo.setPayType(order.getPayType());
-                    carDetailVo.setOrderId(order.getId());
-                    carDetailVo.setOrderNo(order.getNo());
 
                     // 设置任务单车辆ID和运单车辆状态
                     carDetailVo.setId(taskCar.getId());
