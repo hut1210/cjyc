3f60d708c165878535de59a3db0e611866db236b
runoob <zhangyao198800@gmail.com>
2020-02-12 11:15:06 +0800

    导入订单查询省市区编码逻辑修改

--- cjyc-web-api/src/main/java/com/cjyc/web/api/util/CustomerOrderExcelVerifyHandler.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/util/CustomerOrderExcelVerifyHandler.java
@@ -5,10 +5,12 @@ import cn.afterturn.easypoi.handler.inter.IExcelVerifyHandler;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.cjyc.common.model.dto.web.order.ImportCustomerOrderDto;
 import com.cjyc.common.model.entity.City;
+import com.cjyc.common.model.enums.city.CityLevelEnum;
 import com.cjyc.web.api.service.ICityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 
@@ -25,21 +27,23 @@ public class CustomerOrderExcelVerifyHandler implements IExcelVerifyHandler<Impo
     public ExcelVerifyHandlerResult verifyHandler(ImportCustomerOrderDto dto) {
         ExcelVerifyHandlerResult result = new ExcelVerifyHandlerResult(true);
         //始发城市
-        City stProvince = getCityLikeName(dto.getStartProvince());
+        City stProvince = getCityLikeName(dto.getStartProvince(), CityLevelEnum.PROVINCE.code, null);
         if (null == stProvince) {
             result.setSuccess(false);
             result.setMsg("始发城市(省)名称有误");
         } else {
             dto.setStartProvinceCode(stProvince.getCode());
         }
-        City stCity = getCityLikeName(dto.getStartCity());
+        City stCity = getCityLikeName(dto.getStartCity(), CityLevelEnum.CITY.code,
+                dto.getStartProvinceCode());
         if (null == stCity) {
             result.setSuccess(false);
             result.setMsg("始发城市(市)名称有误");
         } else {
             dto.setStartCityCode(stCity.getCode());
         }
-        City stArea = getCityLikeName(dto.getStartArea());
+        City stArea = getCityLikeName(dto.getStartArea(), CityLevelEnum.AREA.code,
+                dto.getStartCityCode());
         if (null == stArea) {
             result.setSuccess(false);
             result.setMsg("始发城市(区/县)名称有误");
@@ -47,21 +51,24 @@ public class CustomerOrderExcelVerifyHandler implements IExcelVerifyHandler<Impo
             dto.setStartAreaCode(stArea.getCode());
         }
         //到达城市
-        City endProvince = getCityLikeName(dto.getEndProvince());
+        City endProvince = getCityLikeName(dto.getEndProvince(), CityLevelEnum.PROVINCE.code,
+                null);
         if (null == endProvince) {
             result.setSuccess(false);
             result.setMsg("目的城市(省)名称有误");
         } else {
             dto.setEndProvinceCode(endProvince.getCode());
         }
-        City endCity = getCityLikeName(dto.getEndCity());
+        City endCity = getCityLikeName(dto.getEndCity(), CityLevelEnum.CITY.code,
+                dto.getEndProvinceCode());
         if (null == endCity) {
             result.setSuccess(false);
             result.setMsg("目的城市(市)名称有误");
         } else {
             dto.setEndCityCode(endCity.getCode());
         }
-        City endArea = getCityLikeName(dto.getEndArea());
+        City endArea = getCityLikeName(dto.getEndArea(), CityLevelEnum.AREA.code,
+                dto.getEndCityCode());
         if (null == endArea) {
             result.setSuccess(false);
             result.setMsg("目的城市(区/县)名称有误");
@@ -76,9 +83,11 @@ public class CustomerOrderExcelVerifyHandler implements IExcelVerifyHandler<Impo
      * @param name
      * @return null: 查询失败 City对象：查询成功，实体对象
      */
-    private City getCityLikeName(String name) {
+    private City getCityLikeName(String name, Integer level, String parentCode) {
         List<City> cityList = cityService.list(new QueryWrapper<City>().lambda()
-                .like(City::getName, name));
+                .like(City::getName, name)
+                .eq(level != null && level > 0, City::getLevel, level)
+                .eq(!StringUtils.isEmpty(parentCode), City::getParentCode, parentCode));
         if (CollectionUtils.isEmpty(cityList)) {
            return null;
         }
