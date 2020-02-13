1ca4a3216868087e1db9970874a856dd6fde259f
hut1210 <752705907@qq.com>
2020-02-13 18:10:27 +0800

    财务修改

--- cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/FinanceServiceImpl.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/FinanceServiceImpl.java
@@ -489,6 +489,7 @@ public class FinanceServiceImpl implements IFinanceService {
         }
         payableSettlementVo.setPayableTaskVo(settlementVoList);
         payableSettlementVo.setTotalFreightFee(freightFee.divide(new BigDecimal(100)));
+        payableSettlementVo.setTotalFreightPaid(settlementVo.getTotalFreightPay());
         payableSettlementVo.setWriteOffTime(settlementVo.getWriteOffTime());
 
         return BaseResultUtil.success(payableSettlementVo);
