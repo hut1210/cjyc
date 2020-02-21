0153b6faa119f934f6e56994d7fe1320a1be9d77
liuxingxiang <liuxingxiang>
2020-02-21 15:14:12 +0800

    修改接口

--- cjyc-customer-api/src/main/java/com/cjyc/customer/api/service/impl/OrderServiceImpl.java
+++ cjyc-customer-api/src/main/java/com/cjyc/customer/api/service/impl/OrderServiceImpl.java
@@ -117,7 +117,7 @@ public class OrderServiceImpl extends ServiceImpl<IOrderDao,Order> implements IO
         List<OrderCenterVo> list = orderDao.selectPage(dto);
 
         // 刪除车辆信息为空的元素
-        list.removeIf(item -> CollectionUtils.isEmpty(item.getOrderCarCenterVoList()));
+        //list.removeIf(item -> CollectionUtils.isEmpty(item.getOrderCarCenterVoList()));
 
         PageInfo<OrderCenterVo> pageInfo = new PageInfo<>(list);
         Map<String, Object> orderCount = getOrderCount(dto.getLoginId());
