e4ec87659e2864c71e0a65ad6aaad5c0243575f9
liuxingxiang <liuxingxiang>
2020-03-10 10:23:22 +0800

    删除测试类

--- cjyc-foreign-api/src/main/java/com/cjyc/foreign/api/service/ITestService.java
+++
@@ -1,8 +0,0 @@
-package com.cjyc.foreign.api.service;
-
-import com.baomidou.mybatisplus.extension.service.IService;
-import com.cjyc.foreign.api.entity.Line;
-
-public interface ITestService extends IService<Line> {
-    Line getLineById(Long id);
-}
