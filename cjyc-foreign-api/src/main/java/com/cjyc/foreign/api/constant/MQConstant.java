39afc908747816dc7bf9fb47d03e293db39cd81b
zhangcangman <zhangyao1988@126.com>
2020-03-16 14:22:56 +0800

    韵车对外提供接口服务: 接口开发

--- cjyc-foreign-api/src/main/java/com/cjyc/foreign/api/constant/MQConstant.java
+++ cjyc-foreign-api/src/main/java/com/cjyc/foreign/api/constant/MQConstant.java
@@ -7,14 +7,14 @@ public class MQConstant {
     /**
      * 韵车推送队列
      */
-    public static final String QUEUE_YC_PUSH = "yc_push_queue";
+    public static final String QUEUE_YC_PUSH = "cjyc_route_common_key";
     /**
-     * 订单状态主题
+     * 99车圈订单状态
      */
-    public static final  String TOPIC_ORDER_STATE = "order_state_99";
+    public static final  String TOPIC_ORDER_STATE_99CC = "99CC_order_state";
 
     /**
-     * 订单确认主题
+     * 99车圈订单确认
      */
-    public static final String TOPIC_ORDER_CONFIRM = "order_confirm_99";
+    public static final String TOPIC_ORDER_CONFIRM_99CC = "99CC_order_confirm";
 }
