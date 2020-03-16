5f9eef2ecf8c198f1a9d048a929cfbfe028e5f5c
liuxingxiang <liuxingxiang>
2020-03-16 09:01:09 +0800

    更新

--- cjyc-foreign-api/src/main/java/com/cjyc/foreign/api/dto/req/OrderCarSubmitReqDto.java
+++ cjyc-foreign-api/src/main/java/com/cjyc/foreign/api/dto/req/OrderCarSubmitReqDto.java
@@ -19,27 +19,27 @@ public class OrderCarSubmitReqDto implements Serializable {
     private static final long serialVersionUID = -3441938847460577267L;
 
     @NotBlank(message = "不能为空")
-    @ApiModelProperty(value = "品牌")
+    @ApiModelProperty(value = "品牌", required = true)
     private String brand;
 
     @NotBlank(message = "不能为空")
-    @ApiModelProperty(value = "型号")
+    @ApiModelProperty(value = "型号", required = true)
     private String model;
 
     @Pattern(regexp = "(^$)|(^\\S{1,20}$)", message = "车牌号格式不正确，请检查")
-    @ApiModelProperty(value = "车牌号")
+    @ApiModelProperty(value = "车牌号", required = true)
     private String plateNo;
 
-    @Pattern(regexp = "(^$)|(^[0-9a-zA-Z]{1,20}$)", message = "vin码格式不正确，请检查")
+    //@Pattern(regexp = "(^$)|(^[0-9a-zA-Z]{1,20}$)", message = "vin码格式不正确，请检查")
     @ApiModelProperty(value = "vin码")
     private String vin;
 
     @NotNull(message = "是否能动标识不能为空")
-    @ApiModelProperty(value = "是否能动 0-否 1-是")
+    @ApiModelProperty(value = "是否能动 0-否 1-是", required = true)
     private Integer isMove;
 
     @NotNull(message = "是否新车标识不能为空")
-    @ApiModelProperty(value = "是否新车 0-否 1-是")
+    @ApiModelProperty(value = "是否新车 0-否 1-是", required = true)
     private Integer isNew;
 
     @ApiModelProperty(value = "车值/万")
