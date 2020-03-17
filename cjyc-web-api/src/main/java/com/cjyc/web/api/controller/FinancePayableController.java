185d2399560dd4da50166538190c5cc6640044cc
renpengliang <renpengliang@changjiulogistics.com>
2020-03-17 13:44:30 +0800

    司机时付-上游付款状态接口sql修改

--- cjyc-web-api/src/main/java/com/cjyc/web/api/controller/FinancePayableController.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/controller/FinancePayableController.java
@@ -206,7 +206,7 @@ public class FinancePayableController {
     }
 
     @ApiOperation(value = "应付账款-司机时付-根据运单号查看上游付款状态列表")
-    @PostMapping(value = "/listDriverUpstreamPaidInfo")
+    @PostMapping(value = "/listDriverUpstreamPaidInfo/{waybillNo}")
     public ResultVo<PageVo<DriverUpstreamPaidInfoVo>> listDriverUpstreamPaidStatus(@PathVariable String waybillNo){
         return financeService.listDriverUpstreamPaidInfo(waybillNo);
     }
