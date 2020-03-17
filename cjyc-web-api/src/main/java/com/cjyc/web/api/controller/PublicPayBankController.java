f646650f748a69755c062f9b31a89f3bc09f6dfc
zhengjiajia <zjj5710109>
2020-03-17 16:32:51 +0800

    Merge branches 'develop' and 'yc_auth_20200102' of http://10.253.96.55/CJYC/cjyc into yc_auth_20200102
    # Conflicts:
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/ICsCustomerService.java

--- cjyc-web-api/src/main/java/com/cjyc/web/api/controller/PublicPayBankController.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/controller/PublicPayBankController.java
@@ -25,6 +25,7 @@ public class PublicPayBankController {
 
     @ApiOperation(value = "对公支付行号导入Excel", notes = "\t 请求接口为/importPayBankExcel/loginId(登录用户ID)格式")
     @PostMapping("/importPayBankExcel/{loginId}")
+    @Deprecated
     public ResultVo importPayBankExcel(@RequestParam("file") MultipartFile file, @PathVariable Long loginId){
         boolean result = payBankService.importPayBankExcel(file,loginId);
         return result ? BaseResultUtil.success() : BaseResultUtil.fail(ResultEnum.FAIL.getMsg());
