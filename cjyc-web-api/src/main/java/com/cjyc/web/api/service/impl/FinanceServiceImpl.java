78d9ca4f6c3ea5b8f15d6ebc78ec3658591e5d3c
huteng <96531210>
2020-03-18 08:51:44 +0800

    Merge branch 'yc_auth_20200102' of D:\IDEA\project\cjwl\cjyc with conflicts.

--- cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/FinanceServiceImpl.java
+++ cjyc-web-api/src/main/java/com/cjyc/web/api/service/impl/FinanceServiceImpl.java
@@ -15,6 +15,8 @@ import com.cjyc.common.system.service.ICsSendNoService;
 import com.cjyc.web.api.service.ICustomerService;
 import com.cjyc.web.api.service.IFinanceService;
 import com.cjyc.web.api.service.IOrderService;
+import com.cjyc.web.api.service.ITradeBillSummaryService;
+import com.fasterxml.jackson.databind.ser.Serializers;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 import lombok.extern.slf4j.Slf4j;
@@ -85,6 +87,9 @@ public class FinanceServiceImpl implements IFinanceService {
     @Resource
     private IWaybillCarDao waybillCarDao;
 
+    @Resource
+    private ITradeBillSummaryService tradeBillService;
+
     @Override
     public ResultVo<PageVo<FinanceVo>> getFinanceList(FinanceQueryDto financeQueryDto) {
         PageHelper.startPage(financeQueryDto.getCurrentPage(), financeQueryDto.getPageSize());
@@ -143,7 +148,18 @@ public class FinanceServiceImpl implements IFinanceService {
             }
         }
         PageInfo<FinanceVo> pageInfo = new PageInfo<>(financeVoList);
-        return BaseResultUtil.success(pageInfo);
+        BigDecimal incomeSummary = tradeBillService.incomeSummary(financeQueryDto);
+
+        BigDecimal costSummary = tradeBillService.costSummary(financeQueryDto);
+
+        BigDecimal grossProfit = tradeBillService.grossProfit(financeQueryDto);
+
+        Map<String, Object> countInfo = new HashMap<>();
+        countInfo.put("incomeSummary", incomeSummary.divide(new BigDecimal(100)));
+        countInfo.put("costSummary",costSummary.divide(new BigDecimal(100)));
+        countInfo.put("grossProfit",grossProfit.divide(new BigDecimal(100)));
+
+        return BaseResultUtil.success(pageInfo,countInfo);
     }
 
     @Override
@@ -360,9 +376,11 @@ public class FinanceServiceImpl implements IFinanceService {
         FinanceQueryDto fqd = new FinanceQueryDto();
         List<PaymentVo> pv = financeDao.getPaymentList(fqd);
 
+        BigDecimal receiptSummary = tradeBillService.receiptSummary(financeQueryDto);
         log.info("pv.size() ={}", pv.size());
         Map<String, Object> countInfo = new HashMap<>();
         countInfo.put("receiptCount", pv.size());
+        countInfo.put("receiptSummary",receiptSummary.divide(new BigDecimal(100)));
         return BaseResultUtil.success(pageInfo, countInfo);
     }
 
@@ -617,7 +635,17 @@ public class FinanceServiceImpl implements IFinanceService {
         }
         log.info("financeVoList = " + financeVoList.size());
         PageInfo<PaidNewVo> pageInfo = new PageInfo<>(financeVoList);
-        return BaseResultUtil.success(pageInfo, getCountInfo());
+
+        Map countInfo = getCountInfo();
+
+        //承运商应付款汇总
+        BigDecimal carrierSummary = tradeBillService.payToCarrierSummary(payMentQueryDto);
+        countInfo.put("carrierSummary",carrierSummary.divide(new BigDecimal(100)));
+        //承运商实付款汇总
+        BigDecimal paidCarrierSummary = tradeBillService.paidToCarrierSummary(payMentQueryDto);
+        countInfo.put("paidCarrierSummary",paidCarrierSummary.divide(new BigDecimal(100)));
+
+        return BaseResultUtil.success(pageInfo, countInfo);
     }
 
     private List<PaidNewVo> getAutoPaidList(PayMentQueryDto payMentQueryDto) {
@@ -803,7 +831,16 @@ public class FinanceServiceImpl implements IFinanceService {
 
         }
         PageInfo<CooperatorPaidVo> pageInfo = new PageInfo<>(cooperatorPaidVoList);
-        return BaseResultUtil.success(pageInfo, getCountInfo());
+
+        Map countInfo = getCountInfo();
+
+        //合伙人应付汇总
+        BigDecimal payCooperatorSummary = tradeBillService.payToCooperatorSummary(cooperatorSearchDto);
+        countInfo.put("payCooperatorSummary",payCooperatorSummary.divide(new BigDecimal(100)));
+        //合伙人实付汇总
+        BigDecimal paidCooperatorSummary = tradeBillService.paidToCooperatorSummary(cooperatorSearchDto);
+        countInfo.put("paidToCooperatorSummary",paidCooperatorSummary.divide(new BigDecimal(100)));
+        return BaseResultUtil.success(pageInfo, countInfo);
     }
 
     @Override
