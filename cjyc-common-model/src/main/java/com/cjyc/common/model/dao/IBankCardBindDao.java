20fa50db6cf8730872531ee386dbe9a88579b370
zhengjiajia <zjj5710109>
2020-03-18 14:23:00 +0800

    Merge branches 'develop' and 'yc_auth_20200102' of http://10.253.96.55/CJYC/cjyc into yc_auth_20200102
    # Conflicts:
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/ICsCustomerService.java

--- cjyc-common-model/src/main/java/com/cjyc/common/model/dao/IBankCardBindDao.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/dao/IBankCardBindDao.java
@@ -2,6 +2,7 @@ package com.cjyc.common.model.dao;
 
 import com.cjyc.common.model.entity.BankCardBind;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.cjyc.common.model.vo.customer.customerInfo.CustomerCardInfoVo;
 import com.cjyc.common.model.vo.driver.mine.BankCardVo;
 import org.apache.ibatis.annotations.Param;
 
@@ -37,5 +38,12 @@ public interface IBankCardBindDao extends BaseMapper<BankCardBind> {
      * @return
      */
     int findBankCardInfoNum(@Param("carrierId") Long carrierId);
+
+    /**
+     * 获取用户银行卡信息
+     * @param loginId
+     * @return
+     */
+    CustomerCardInfoVo findCustomerBankCardInfo(@Param("loginId") Long loginId);
 
 }
