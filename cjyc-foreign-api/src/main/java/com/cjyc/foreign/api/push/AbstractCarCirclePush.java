d322caf86648bd909cc1a52fdef128de545f7b04
zhangcangman <zhangyao1988@126.com>
2020-03-19 09:11:56 +0800

    韵车对外提供接口服务: 接口开发

---
+++ cjyc-foreign-api/src/main/java/com/cjyc/foreign/api/push/AbstractCarCirclePush.java
@@ -0,0 +1,50 @@
+package com.cjyc.foreign.api.push;
+
+import com.alibaba.fastjson.JSONObject;
+import com.cjyc.foreign.api.utils.HttpClientUtil;
+import com.cjyc.foreign.api.utils.MD5Util;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author zcm
+ * @date 2020/3/19 8:29
+ */
+public abstract class AbstractCarCirclePush implements IPushable{
+    //返回接口key
+    private static final String RESULT_KEY = "success";
+    
+    /**
+    * @Description: 韵车接口请求及验证返回结果
+    * @Param: [interUrl, data, charset, headers]
+    * @return: boolean
+    * @Author: zcm
+    * @Date: 2020/3/19
+    */
+    protected boolean doPush(String interUrl, JSONObject jo, String clientId, String charset,
+                                        Map<String, String> headers) {
+        StringBuilder sb = new StringBuilder("");
+        sb.append("createTime" + jo.get("createTime"));
+        sb.append("orderNo" + jo.get("orderNo"));
+        sb.append("clientId" + clientId);
+        String md5Str = MD5Util.getMD5Str(sb.toString());
+        if (StringUtils.isEmpty(md5Str)) {
+            return false;
+        }
+        jo.put("sign", md5Str);
+        System.out.println("新版_待加密内容：" + sb.toString());
+        System.out.println("新版_接收到处理后数据为: " + jo.toJSONString());
+//        String result = HttpClientUtil.sendJsonPost(interUrl, jo.toJSONString(), null, null);
+//        if (StringUtils.isEmpty(result)) {
+//            throw new RuntimeException("请求接口: " + interUrl + "异常");
+//        }
+//        JSONObject resultJo = JSONObject.parseObject(result);
+//        if (resultJo.containsKey(RESULT_KEY) && resultJo.getBoolean(RESULT_KEY)) {
+//            return true;
+//        }
+        return false;
+    }
+}
--- cjyc-foreign-api/src/main/java/com/cjyc/foreign/api/push/IOrderConfirmPushable.java
+++
@@ -1,7 +0,0 @@
-package com.cjyc.foreign.api.push;
-
-/**
- * 订单确认推送
- */
-public interface IOrderConfirmPushable extends IPushable{
-}
