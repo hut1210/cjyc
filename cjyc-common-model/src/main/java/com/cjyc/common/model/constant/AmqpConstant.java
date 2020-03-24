f5718603d97d0bc35a0dd44aa251b17797d499d7
鞠培国 <jupeiguo@changjiulogistics.com>
2020-03-24 15:20:33 +0800

    导出改价信息

--- cjyc-common-model/src/main/java/com/cjyc/common/model/constant/AmqpConstant.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/constant/AmqpConstant.java
@@ -2,5 +2,5 @@ package com.cjyc.common.model.constant;
 
 public class AmqpConstant {
     public final static String MQ_TYPE_99CC_ORDER_STATE = "99CC_order_state";
-    public final static String MQ_TYPE_99CC_ORDER_CONFIRM = "99CC_order_state";
+    public final static String MQ_TYPE_99CC_ORDER_CONFIRM = "99CC_order_confirm";
 }
