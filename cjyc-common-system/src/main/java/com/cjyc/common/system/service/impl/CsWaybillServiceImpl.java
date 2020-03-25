7bb4677fbd90aaf3ad92670e4af67e262c23a322
鞠培国 <jupeiguo@changjiulogistics.com>
2020-03-25 15:43:10 +0800

    送车调度验证始发地，而不是目的地

--- cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsWaybillServiceImpl.java
+++ cjyc-common-system/src/main/java/com/cjyc/common/system/service/impl/CsWaybillServiceImpl.java
@@ -194,8 +194,8 @@ public class CsWaybillServiceImpl implements ICsWaybillService {
                 //【验证】配送调度，需验证干线调度是否完成
                 if (paramsDto.getType() == WaybillTypeEnum.BACK.code) {
                     WaybillCar waybillCar = waybillCarDao.findLastWaybillCar(orderCarId);
-                    String endAreaCode = waybillCar == null ? order.getStartAreaCode() : waybillCar.getEndAreaCode();
-                    String endCityCode = waybillCar == null ? order.getStartCityCode() : waybillCar.getEndCityCode();
+                    String endAreaCode = waybillCar == null ? order.getStartAreaCode() : waybillCar.getStartAreaCode();
+                    String endCityCode = waybillCar == null ? order.getStartCityCode() : waybillCar.getStartCityCode();
                     Long endStoreId = waybillCar == null ? order.getStartStoreId() : waybillCar.getEndStoreId();
                     if (!csOrderService.validateIsArriveStoreOrCityRange(endStoreId, endAreaCode, endCityCode, order.getEndStoreId(), order.getEndCityCode())) {
                         return BaseResultUtil.fail("车辆{0}，尚未到达目的地所属业务中心或目的地城市范围内", orderCarNo);
