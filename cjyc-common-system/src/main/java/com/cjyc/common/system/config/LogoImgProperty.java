c305436193437051ff2bedaa72faaf3e8bedcd21
zhengjiajia <zjj5710109>
2020-03-31 08:31:25 +0800

    Merge branches 'develop' and 'yc_auth_20200102' of http://10.253.96.55/CJYC/cjyc into yc_auth_20200102
    # Conflicts:
    #	cjyc-common-system/src/main/java/com/cjyc/common/system/service/ICsCustomerService.java

--- cjyc-common-system/src/main/java/com/cjyc/common/system/config/LogoImgProperty.java
+++ cjyc-common-system/src/main/java/com/cjyc/common/system/config/LogoImgProperty.java
@@ -7,21 +7,10 @@ import org.springframework.context.annotation.Configuration;
 public class LogoImgProperty {
 
     public static String logoImg;
-    public static String apiKey;
-    public static String url;
 
     @Value("${cjyc.carSeries.logoImg}")
     public void setLogoImg(String logoImg) {
         LogoImgProperty.logoImg = logoImg;
     }
 
-    @Value("${cjyc.carSeries.apiKey}")
-    public void setApiKey(String apiKey) {
-        LogoImgProperty.apiKey = apiKey;
-    }
-
-    @Value("${cjyc.carSeries.url}")
-    public void setUrl(String url) {
-        LogoImgProperty.url = url;
-    }
 }
