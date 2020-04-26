c519eb5f31acdd6f1ba16243bded7d00f85215e4
鞠培国 <jupeiguo@changjiulogistics.com>
2020-04-26 11:46:01 +0800

    web端提送车运单可修改，兼容之前版本（未传值按照之前的逻辑）

--- cjyc-common-model/src/main/java/com/cjyc/common/model/dto/web/waybill/SaveLocalDto.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/dto/web/waybill/SaveLocalDto.java
@@ -1,6 +1,5 @@
 package com.cjyc.common.model.dto.web.waybill;
 
-import com.cjyc.common.model.constant.ArgsConstant;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
@@ -9,11 +8,8 @@ import lombok.experimental.Accessors;
 import org.springframework.validation.annotation.Validated;
 
 import javax.validation.Valid;
-import javax.validation.constraints.DecimalMin;
-import javax.validation.constraints.Digits;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.math.BigDecimal;
 import java.util.List;
 
 @Data
@@ -33,12 +29,6 @@ public class SaveLocalDto {
     @NotNull(message = "调度类型不能为空")
     @ApiModelProperty(value = "运单类型：1提车运单，2干线运单，3送车运单", required = true)
     private Integer type;
-
-    @ApiModelProperty(value = "运费", required = true)
-    @Digits(integer = ArgsConstant.INT_MAX, fraction = ArgsConstant.FRACTION_MAX, message = "金额整数最多8位，小数最多2位")
-    @DecimalMin(value = ArgsConstant.DECIMAL_ZERO, message = "金额不能小于0")
-    private BigDecimal freightFee;
-
 
     @NotEmpty(message = "调度内容不能为空")
     @ApiModelProperty(value = "调度运单列表", required = true)
