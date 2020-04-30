a449361de88e1609a7f0e1f611a3712e78130cc9
鞠培国 <jupeiguo@changjiulogistics.com>
2020-04-30 10:37:29 +0800

    子运单显示运费

--- cjyc-common-model/src/main/java/com/cjyc/common/model/constant/RegexConstant.java
+++ cjyc-common-model/src/main/java/com/cjyc/common/model/constant/RegexConstant.java
@@ -34,7 +34,7 @@ public class RegexConstant {
      * <p>全球星：1349</p>
      * <p>虚拟运营商：170</p>
      */
-    public static final String REGEX_MOBILE_EXACT_LATEST = "^((13[0-9])|(14[5,7,9])|(15[0-3,5-9])|(166)|(17[0-3,5-8])|(18[0-9])|(19[8,9]))\\d{8}$";
+    public static final String REGEX_MOBILE_EXACT_LATEST = "^((13[0-9])|(14[5,7,9])|(15[0-3,5-9])|(166)|(17[0-3,5-8])|(18[0-9])|(19[1,8,9]))\\d{8}$";
 
     /**
      * 正则：电话号码
